fcec5d2126fc733c8877bc0799646c00e4456ae2
airvip <sdqhwzb@163.com>
2023-03-28 14:49:54 +0800

    springboot3 init

---
+++ src/main/java/wang/diff/user/server/UserServerApplication.java
@@ -0,0 +1,11 @@
+package wang.diff.user.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+}
---
+++ src/main/java/wang/diff/user/server/controller/api/PingController.java
@@ -0,0 +1,19 @@
+package wang.diff.user.server.controller.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import diff.wang.user.server.controller.PingApi;
+
+@RestController
+@Tag(name = "Ping")
+public class PingController implements PingApi {
+
+    @Override
+    public ResponseEntity<String> ping(@Valid String name) {
+        return ResponseEntity.ok("ping "+name);
+    }
+}
